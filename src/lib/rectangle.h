8eefeda8841b3b7fa62e5c76c7085711d48a418b
Ivan Guerreschi <ivanguerreschi@gmail.com>
2020-01-08 16:21:16 +0100

    Create header file rectangle

---
+++ src/lib/rectangle.h
@@ -0,0 +1,13 @@
+#ifndef RECTANGLE_H
+#define RECTANGLE_H
+
+float perimeter (float h, float a, float b);
+float area (float a, float b);
+float side_a (float area, float side_b);
+float side_b (float area, float side_a);
+float hypotenuse (float area, float height);
+float hypotenuse_p (float side_a, float side_b);
+float side_a_p (float hypotenuse, float side_b);
+float side_b_p (float hypotenuse, float side_a);
+
+#endif /* RECTANGLE_H */
